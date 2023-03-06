f216aa64709eed0611573c35cb49a91a26f61698
P1ST-0N <luckylionia@gmail.com>
2023-03-07 00:39:47 +0200

    add profile

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-import Section from "./components/Section";
+import Section from './components/Section';
+import SocialProfile from './components/SocialProfile';
+
+import user from './data/user.json';
 
 export default function App() {
   return (
-<Section>
-  <div>dfgsdfg</div>
-</Section>
+    <div>
+      <Section title={"Task-1 'Social network profile'"}>
+        <SocialProfile
+          username={user.username}
+          tag={user.tag}
+          location={user.location}
+          avatar={user.avatar}
+          stats={user.stats}
+        />
+      </Section>
+    </div>
   );
-};
+}
