ad9f321144dfc7b684b7c1b2a32ba8d017a058fc
P1ST-0N <luckylionia@gmail.com>
2023-03-06 19:08:40 +0200

    add Container

---
+++ src/components/App.jsx
@@ -0,0 +1,10 @@
+import { Container } from "./Container";
+
+export
+const App = () => {
+  return (
+    <Container>
+      ///
+    </Container>
+  )
+};
---
+++ src/components/Container/Container.styled.jsx
@@ -0,0 +1,9 @@
+import styled from '@emotion/styled';
+
+const Main = styled.div`
+  max-width: 1200px;
+  margin: 0 auto;
+  padding: 40px;
+`;
+
+export { Main };
