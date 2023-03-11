a50fe69af29983b562e102b2935576cb09887a61
P1ST-0N <luckylionia@gmail.com>
2023-03-11 13:38:40 +0200

    add FrienList

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,11 @@
 import { Profile } from './Profile/Profile';
 import { Statistics } from './Statistics/Statistics';
-
+// import
 import { GlobalStyle } from './GlobalStyle';
 
 import user from '../data/user.json';
 import data from '../data/data.json';
+import friends from '../data/friends.json';
 
 export const App = () => {
   return (
@@ -19,6 +20,8 @@ export const App = () => {
 
 <Statistics title="Upload stats" stats={data} />
 {/* <Statistics stats={data} /> */}
+
+{/* <FriendList friends={friends} /> */}
 
 <GlobalStyle />
     </div>
---
+++ src/components/FriendList/FriendList.jsx
@@ -0,0 +1,9 @@
+import PropTypes from 'prop-types';
+
+
+export const FriendList = ({friends}) => {
+
+  return (<List>{}
+
+  </List>);
+};
