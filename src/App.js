ae68ebb681d509cd9b07b555fef6e9d561fde785
P1ST-0N <luckylionia@gmail.com>
2023-03-15 23:02:59 +0200

    add TransactionHistory component

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import Section from './components/Section';
 import SocialProfile from './components/SocialProfile';
 import Statistics from 'components/Statistics/Statistics';
 import FriendList from 'components/FriendList/FriendList';
+import TransactionHistory from 'components/TransactionHistory/TransactionHistory';
 
 import user from './data/user.json';
 import uploadStats from './data/uploadStats.json';
 import friends from './data/friends.json';
+import transactions from './data/transactions.json';
 
 export default function App() {
   return (
@@ -27,6 +29,10 @@ export default function App() {
 
       <Section title={"Task-3 'Friend list'"}>
         <FriendList friends={friends} />
+      </Section>
+
+      <Section>
+      <TransactionHistory items={transactions} />
       </Section>
     </div>
   );
