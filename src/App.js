ded2863e276f324c46720651695570cafe253b3d
P1ST-0N <luckylionia@gmail.com>
2023-03-20 15:05:15 +0200

    added Feedback component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import Container from "./components/Container/Container";
 import Section from "./components/Section/Section";
+import Statistics from "./components/Statistics/Statistics";
+import Feedback from "./components/Feedback/Feedback";
 
 const App = () => {
   const [stats, setStats] = useState({
@@ -11,11 +13,11 @@ const App = () => {
 
   const {good, neutral, bad} = stats;
 
-  // const handleIncrementStat = ({target: {textContent} }) => 
-  // setStats((prState) => ({
-  //   ...prState,
-  //   [textContent]: stats[textContent] + 1,
-  // }));
+  const handleIncrementStat = ({target: {textContent} }) => 
+  setStats((prState) => ({
+    ...prState,
+    [textContent]: stats[textContent] + 1,
+  }));
 
 const countTotalFeedback = () => {
 const result = good + neutral + bad;
@@ -28,9 +30,21 @@ Math.round(((good * 100) / countTotalFeedback()) * 10) / 10;
   return (
     <Container>
       <Section title="Please leave feedback">
-        <div>2143434sdgdsf</div>
-        {/* <FeedbackOptions /> */}
+        <Feedback
+        options={Object.keys(stats)}
+        onLeaveFeedback={handleIncrementStat}
+        />
       </Section>
+
+      <Section title="Statistics">
+      <Statistics 
+      good={good} 
+      neutral={neutral} 
+      bad={bad} 
+      total={countTotalFeedback} 
+      positivePercentage={countPositiveFeedbackPercentage}
+      />
+     </Section>
     </Container>
   );
 };
