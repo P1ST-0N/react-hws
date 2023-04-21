3646b99a3338e29beae8d4c8387f4d5993e1c3fb
P1ST-0N <luckylionia@gmail.com>
2023-04-22 02:46:00 +0300

    added PixabayAPI component

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import { Component } from "react";
 import { Searchbar } from "./Searchbar/Searchbar";
+import { apiRequest } from "./PixabayAPI/PixabayAPI";
 
 export class App extends Component {
   state = {
---
+++ src/components/PixabayAPI/PixabayAPI.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+axios.defaults.baseURL = 'https://pixabay.com/api/';
+const key = '29443951-f0fab2e7bdcd1423b667d2410';
+
+export const apiRequest = async (query, page) => {
+    const response = await axios.get(
+        `?q=${query}&page=${page}&key=${key}&image_type=photo&orientation=horizontal&per_page=12`
+    );
+    console.log(response);
+
+    if (response.data.hits.length === 0) {
+        alert(`${query} не знайдено :(`);
+    }
+
+    return response.data;
+};
