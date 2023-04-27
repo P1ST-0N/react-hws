f3236b9b60d2cd7ab025c9f9e316bd0a7de9b452
P1ST-0N <luckylionia@gmail.com>
2023-04-27 17:49:17 +0300

    added Loader & Button components

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,48 @@
 import { Component } from "react";
 import { Searchbar } from "./Searchbar/Searchbar";
-import { apiRequest } from './PixabayAPI/PixabayAPI';
+import { apiRequest } from "./PixabayAPI/PixabayAPI";
 import { ImageGallery } from "./ImageGallery/ImageGallery";
+import { Loader } from "./Loader/Loader";
+import { Button } from "./Button/Button";
 
 export class App extends Component {
   state = {
-    query: '',
+    query: "",
     page: 1,
     hits: [],
     totalHits: 0,
     loading: false,
   };
 
-  updateQuery = query => {
+  componentDidUpdate(prevProps, prevState) {
+    const { query, page } = this.state;
+    if (prevState.query !== query) {
+      this.formFetch(query);
+    }
+
+    if (query === "") {
+      return alert("Поле пошуку не може бути пустим!");
+    }
+
+    if (prevState.page !== page && page !== 1) {
+      this.updatePage(prevState, page);
+    }
+  }
+
+  formFetch = async (query) => {
+    if (query === "") {
+      return;
+    } else this.setState({ loading: true });
+    const { totalHits, hits } = await apiRequest(query, 1);
+    this.setState({ totalHits, hits, page: 1, loading: false });
+  };
+
+  updateQuery = (query) => {
     this.setState({ query });
+  };
+
+  loadMore = () => {
+    this.setState(({ page }) => ({ page: page + 1 }));
   };
 
   updatePage = async (prevState, page) => {
@@ -22,8 +51,8 @@ export class App extends Component {
     const { hits } = await apiRequest(query, page);
     this.setState({ hits: [...prevState.hits, ...hits], page });
     this.setState({ loading: false });
-  }
-  
+  };
+
   render() {
     const { hits, loading, totalHits, page } = this.state;
 
@@ -31,6 +60,8 @@ export class App extends Component {
       <>
         <Searchbar onSubmit={this.updateQuery} />
         <ImageGallery images={hits} />
+        {page < totalHits && <Button loadMore={this.loadMore} />}
+        {loading && <Loader />}
       </>
     );
   }
---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,17 @@
+import { Vortex } from "react-loader-spinner";
+
+export const Loader = () => {
+    return (
+        <div style={{ textAlign: 'center' }}>
+            <Vortex
+            visible={true}
+            height="100"
+            width="100"
+            ariaLabel="vortex-loading"
+            wrapperStyle={{}}
+            wrapperClass="vortex-wrapper"
+            colors={['red', 'green', 'blue', 'yellow', 'orange', 'purple']}
+            />
+        </div>
+    );
+};
