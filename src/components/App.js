3399d9e51befd24a418aef1014a07c81a8d87c6b
P1ST-0N <luckylionia@gmail.com>
2023-05-12 15:28:02 +0300

    create new react-app

--- src/components/App.js
+++ src/components/App.js
@@ -1,34 +1,20 @@
-import { Profile } from './Profile/Profile';
-import { Statistics } from './Statistics/Statistics';
-import { FriendList } from './FriendList/FriendList';
-import { TransactionHistory } from './TransactionHistory/TransactionHistory';
-
-import { GlobalStyle } from './GlobalStyle';
-
-import user from '../data/user.json';
-import data from '../data/data.json';
-import friends from '../data/friends.json';
-import transactions from '../data/transactions.json';
+import { Route, Routes } from "react-router";
+import { Lazy, Suspense } from "react";
 
 export const App = () => {
   return (
-    <div>
-      <Profile
-        username={user.username}
-        tag={user.tag}
-        location={user.location}
-        avatar={user.avatar}
-        stats={user.stats}
-      />
-
-<Statistics title="Upload stats" stats={data} />
-{/* <Statistics stats={data} /> */}
-
-<FriendList friends={friends} />
-
-<TransactionHistory items={transactions} />
-
-<GlobalStyle />
-    </div>
-  )
+    <>
+    <Suspense fallback={<h2>Loading...</h2>}>
+      <Routes>
+        <Route />
+        <Route />
+        <Route>
+          <Route />
+          <Route />
+        </Route>
+        <Route />
+      </Routes>
+    </Suspense>
+    </>
+  );
 };
