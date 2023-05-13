708d54909a13c4e91779b31d747462ad5b40e29d
P1ST-0N <luckylionia@gmail.com>
2023-05-13 15:00:37 +0300

    added Reviews component

--- src/components/App.js
+++ src/components/App.js
@@ -7,6 +7,7 @@ const Home = lazy(() => import('../pages/Home/Home'));
 const Movies = lazy(() => import('../pages/Movies/Movies'));
 const MovieDetails = lazy(() => import('../pages/MovieDetails/MovieDetails'));
 const Cast = lazy(() => import('./Cast/Cast'));
+const Reviews = lazy(() => import('./Reviews/Reviews'));
 
 export const App = () => {
   return (
@@ -17,9 +18,9 @@ export const App = () => {
           <Route path="/movies" element={<Movies />} />
           <Route path="/movies/:movieId" element={<MovieDetails />}>
             <Route path="/cast" element={<Cast />} />
-            <Route />
+            <Route path="reviews" element={<Reviews />} />
           </Route>
-          <Route />
+          <Route path="*" element={<p>Not found</p>} />
         </Routes>
       </Suspense>
     </Layout>
