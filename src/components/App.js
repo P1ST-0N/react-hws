14171ef5e453a5934531cca81c2b38e438489b05
P1ST-0N <luckylionia@gmail.com>
2023-06-14 20:34:25 +0300

    create new react app & added redux folder

--- src/components/App.js
+++ src/components/App.js
@@ -1,34 +1,17 @@
-import { Profile } from './Profile/Profile';
-import { Statistics } from './Statistics/Statistics';
-import { FriendList } from './FriendList/FriendList';
-import { TransactionHistory } from './TransactionHistory/TransactionHistory';
+import ContactForm from './ContactForm/ContactForm';
+import ContactList from './ContactList/ContactList';
+import Filter from './Filter/Filter';
 
-import { GlobalStyle } from './GlobalStyle';
-
-import user from '../data/user.json';
-import data from '../data/data.json';
-import friends from '../data/friends.json';
-import transactions from '../data/transactions.json';
-
-export const App = () => {
+const App = () => {
   return (
     <div>
-      <Profile
-        username={user.username}
-        tag={user.tag}
-        location={user.location}
-        avatar={user.avatar}
-        stats={user.stats}
-      />
-
-<Statistics title="Upload stats" stats={data} />
-{/* <Statistics stats={data} /> */}
-
-<FriendList friends={friends} />
-
-<TransactionHistory items={transactions} />
-
-<GlobalStyle />
+      <h1>Phonebook</h1>
+      <ContactForm />
+      <h2>Contacts</h2>
+      <Filter />
+      <ContactList />
     </div>
-  )
+  );
 };
+
+export default App;
---
+++ src/components/ContactForm/ContactForm.jsx
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+import css from './ContactForm.module.css';
+import { addContact } from 'redux/contacts/contacts-slice';
+
+// import 
+
+import { useSelector, useDispatch } from "react-redux";
+
+const ContactForm = () => {};
+
+export default ContactForm;
